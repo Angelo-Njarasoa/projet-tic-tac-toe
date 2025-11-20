3fe14bd1dfb554a8a14979696c11fb87de02c589
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2025-11-21 00:56:19 +0100

    memorisation des coups du tour par  tour

--- tictactoe.py
+++ tictactoe.py
@@ -2,53 +2,52 @@ import json
 import os
 def ia():
     t=1
-    while t<=10:
-        if t%2==0:
-            signe="x"
+    while t<=9:
+    
+        if os.path.exists("tictac.json") and os.path.getsize("tictac.json") > 0:
+            try:
+                with open("tictac.json", "r", encoding="utf-8") as f:
+                    board = json.load(f)
+            except json.JSONDecodeError:
+        # fichier corrompu → on repart d’une grille vide
+                board = [
+                    [" ", " ", " ", " ", " "],
+                    [" ", " ", " ", " ", " "],
+                    [" ", " ", " ", " ", " "]
+                ]
         else:
-            signe="o"
-        p= input()
+            board = [
+                [" ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " "]
+            ]
+
+        if t%2==0:
+            signe="X"
+        else:
+            signe="O"
+        p= int(input())
         if p==7:
-            board[0[0]]=signe
+            board[0][0]=signe
         elif p==8:
-            board[0[1]]=signe
+            board[0][2]=signe
         elif p==9:
-            board[0][2]=signe 
+            board[0][4]=signe 
         elif p==4:
             board[1][0]=signe 
         elif p == 5:
-            board[1][1] = signe
-        elif p == 6:
             board[1][2] = signe
+        elif p == 6:
+            board[1][4] = signe
         elif p == 1:
             board[2][0] = signe
         elif p == 2:
-            board[2][1] = signe
-        elif p == 3:
             board[2][2] = signe
-        t+=1      
-    fichier = "tictactoe.json"
-
-# Charger les anciennes données si le fichier existe et n'est pas vide
-    if os.path.exists(fichier) and os.path.getsize(fichier) > 0:
-        with open(fichier, "r") as f:
-            board = json.load(f)
-    else:
-        board = [[][][]]  # sinon créer une liste vide
-
-# Exemple : nouvelle donnée venant de ton script
-    nouveau_mot = signe  # <-- ici tu mets ton data actuel
-    if nouveau_mot:  # seulement si ce n'est pas vide
-        board.append(nouveau_mot)
-
-# Réécrire le fichier JSON avec les nouvelles données
-    with open(fichier, "w") as f:
-        json.dump(data, f, separators=(",", ":"))
-
-print("Données mises à jour :", data)
-        print("bingo !")
-        for y in board:
-            print (y)
-    else:
-        print("pas de morpion")
-ia() 
+        elif p == 3:
+            board[2][4] = signe
+        t+=1
+        for row in board:
+            print("".join(row))    
+        with open("tictac.json",'w',encoding='utf-8') as f:
+            json.dump(board,f,ensure_ascii=False)
+ia()
